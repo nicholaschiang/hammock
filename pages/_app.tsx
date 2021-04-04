066f5892f68fbf52184141ea6d2d8c3a682581c6
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-04-04 13:01:57 -0700

    fix: clean deps and move global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,9 +9,6 @@ import { APIError } from 'lib/model/error';
 import { UserContext } from 'lib/context/user';
 import { fetcher } from 'lib/fetch';
 
-import globals from 'styles/globals';
-import theme from 'styles/theme';
-
 /**
  * Installs a service worker and triggers an `/api/account` re-validation once
  * the service worker has been activated and is control of this page (i.e. once
@@ -63,12 +60,125 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
         <NProgress />
         <Component {...pageProps} />
       </SWRConfig>
-      <style jsx global>
-        {globals}
-      </style>
-      <style jsx global>
-        {theme}
-      </style>
+      <style jsx global>{`
+        ::selection {
+          background-color: var(--selection);
+          color: var(--on-background);
+        }
+
+        *,
+        *:before,
+        *:after {
+          box-sizing: inherit;
+        }
+
+        html {
+          height: 100%;
+          box-sizing: border-box;
+          touch-action: manipulation;
+          font-feature-settings: 'rlig' 1, 'calt' 0;
+        }
+
+        body {
+          position: relative;
+          min-height: 100%;
+          margin: 0;
+        }
+
+        html,
+        body {
+          font-family: var(--font-sans);
+          text-rendering: optimizeLegibility;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+          background-color: var(--background);
+          color: var(--on-background);
+        }
+
+        button.reset {
+          border: unset;
+          background: unset;
+          padding: unset;
+          margin: unset;
+          font: unset;
+          text-align: unset;
+          appearance: unset;
+          cursor: pointer;
+        }
+
+        .loading {
+          background-image: linear-gradient(
+            270deg,
+            var(--accents-1),
+            var(--accents-2),
+            var(--accents-2),
+            var(--accents-1)
+          );
+          background-size: 400% 100%;
+          -webkit-animation: loadingAnimation 8s ease-in-out infinite;
+          animation: loadingAnimation 8s ease-in-out infinite;
+        }
+
+        @keyframes loadingAnimation {
+          0% {
+            background-position: 200% 0;
+          }
+          to {
+            background-position: -200% 0;
+          }
+        }
+      `}</style>
+      <style jsx global>{`
+        :root {
+          --font-sans: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+            'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+            'Helvetica Neue', sans-serif;
+          --font-mono: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+
+          --primary: #0070f3;
+          --on-primary: #fff;
+          --background: #fff;
+          --on-background: #000;
+          --error: #b00020;
+          --on-error: #fff;
+
+          --accents-1: #fafafa;
+          --accents-2: #eaeaea;
+          --accents-3: #999;
+          --accents-4: #888;
+          --accents-5: #666;
+          --accents-6: #444;
+
+          --shadow-small: 0 5px 10px rgba(0, 0, 0, 0.12);
+          --shadow-medium: 0 8px 30px rgba(0, 0, 0, 0.12);
+          --shadow-large: 0 30px 60px rgba(0, 0, 0, 0.12);
+
+          --selection: #79ffe1;
+        }
+
+        .dark {
+          --primary: #5aa6ff;
+          --on-primary: #000;
+          --background: #000;
+          --on-background: #fff;
+          --error: #cf6679;
+          --on-error: #000;
+
+          --accents-1: #111;
+          --accents-2: #333;
+          --accents-3: #444;
+          --accents-4: #666;
+          --accents-5: #888;
+          --accents-6: #999;
+
+          --shadow-small: 0 0 0 1px var(--accents-2);
+          --shadow-medium: 0 0 0 1px var(--accents-2);
+          --shadow-large: 0 0 0 1px var(--accents-2);
+
+          --selection: #f81ce5;
+        }
+      `}</style>
     </UserContext.Provider>
   );
 }
