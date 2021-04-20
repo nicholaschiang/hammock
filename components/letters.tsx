77ceb4693296edf05b4eff0d4cf2c4d76f1194ed
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-04-19 21:00:23 -0700

    fix(letters): restore important newsletters list

--- components/letters.tsx
+++ components/letters.tsx
@@ -212,7 +212,7 @@ export default function Letters() {
         </Button>
       </header>
       <div className='line' />
-      {false && !!important.length && (
+      {!!important.length && (
         <ul>
           {important.map((r) => (
             <LetterRow
