3a1c71cab5c695dac52d9d4a27e061c2d09a6c80
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-05-07 20:06:53 -0700

    feat(nav-bar): add overflow pop-up menu
    Fixes #22.

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import cn from 'classnames';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 import Avatar from 'components/avatar';
 
@@ -46,17 +47,17 @@ export function NavButton({ onClick, children }: NavButtonProps): JSX.Element {
   );
 }
 
-export interface NavLinkProps {
+export interface LinkProps {
   href: string;
   children: string;
 }
 
-export function NavLink({ href, children }: NavLinkProps): JSX.Element {
+export function NavLink({ href, children }: LinkProps): JSX.Element {
   const { pathname } = useRouter();
 
   return (
     <Link href={href}>
-      <a className={cn({ active: pathname === href })}>
+      <a className={cn('nowrap', { active: pathname === href })}>
         {children}
         <style jsx>{`
           a {
@@ -68,9 +69,6 @@ export function NavLink({ href, children }: NavLinkProps): JSX.Element {
             font-weight: 400;
             line-height: 24px;
             height: 24px;
-            overflow: hidden;
-            white-space: nowrap;
-            text-overflow: ellipsis;
             margin: 16px 0;
             display: block;
           }
@@ -89,13 +87,63 @@ export function NavLink({ href, children }: NavLinkProps): JSX.Element {
   );
 }
 
+export function MenuLink({ href, children }: LinkProps): JSX.Element {
+  return (
+    <Link href={href}>
+      <a
+        rel={href.startsWith('http') ? 'noopener noreferrer' : undefined}
+        target={href.startsWith('http') ? '_blank' : undefined}
+        className='nowrap'
+      >
+        {children}
+        <style jsx>{`
+          a {
+            display: block;
+            padding: 12px 36px 12px 24px;
+            background: var(--background);
+            transition: background 0.2s ease 0s;
+            color: var(--on-background);
+            text-decoration: none;
+            cursor: pointer;
+            font-size: 16px;
+            font-weight: 400;
+            line-height: 18px;
+          }
+
+          a:hover {
+            background: var(--accents-2);
+          }
+        `}</style>
+      </a>
+    </Link>
+  );
+}
+
 export default function NavBar(): JSX.Element {
   const { loggedIn, user } = useUser();
+  const [open, setOpen] = useState<boolean>(false);
 
   return (
     <div className='wrapper'>
       <div className='content'>
-        <Avatar loading={!loggedIn} src={user.photo} size={48} />
+        <button
+          onClick={() => setOpen((prev) => !prev)}
+          className='reset avatar'
+          type='button'
+        >
+          <Avatar loading={!loggedIn} src={user.photo} size={48} />
+        </button>
+        <div className={cn('menu', { open })}>
+          <MenuLink href='/letters'>Subscriptions</MenuLink>
+          <MenuLink href='/archive'>Archive</MenuLink>
+          <MenuLink href='#'>Settings</MenuLink>
+          <div className='line' />
+          <MenuLink href='#'>Help</MenuLink>
+          <MenuLink href='https://form.typeform.com/to/oTBbAI6z'>
+            Send feedback
+          </MenuLink>
+          <MenuLink href='/api/logout'>Logout</MenuLink>
+        </div>
         <nav>
           <NavLink href='/'>Feed</NavLink>
           <NavLink href='/quick-read'>Quick read</NavLink>
@@ -112,7 +160,34 @@ export default function NavBar(): JSX.Element {
 
         .content {
           position: sticky;
+          z-index: 4;
           top: 96px;
+        }
+
+        .menu {
+          pointer-events: none;
+          transform: translateX(-50%);
+          position: absolute;
+          top: 60px;
+          left: 24px;
+          opacity: 0;
+          padding: 4px 0;
+          overflow: hidden;
+          border-radius: 8px;
+          background: var(--background);
+          box-shadow: var(--shadow-medium);
+          transition: top 0.2s ease 0s, opacity 0.2s ease 0s;
+        }
+
+        .menu.open {
+          pointer-events: unset;
+          top: 72px;
+          opacity: 1;
+        }
+
+        .menu .line {
+          border-top: 1px solid var(--accents-2);
+          margin: 4px 0;
         }
 
         nav {
