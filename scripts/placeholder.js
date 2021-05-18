11e853d77a8210035a75762073bf420b876711c7
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-05-17 21:43:10 -0700

    feat(scripts): add base64 placeholder option

--- scripts/placeholder.js
+++ scripts/placeholder.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 const { getPixelsCSS } = require('@plaiceholder/css');
+const { getBase64 } = require('@plaiceholder/base64');
 
 async function generatePlaceholderCSS(file) {
   const image = fs.readFileSync(file);
   const css = await getPixelsCSS(image);
-  console.log(css);
+  console.log('CSS:', css);
+  const base64 = await getBase64(image);
+  console.log('Base64:', base64);
 }
 
-if (require.main === module) generatePlaceholderCSS('../public/rockets.gif');
+if (require.main === module)
+  generatePlaceholderCSS('../public/images/hammock-webclip.png');
