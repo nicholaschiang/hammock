04e6a5734d776ef53ae8be5255327c6ffc76e521
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-05-18 21:40:36 -0700

    feat(next.config.js): add i18n to set `html` lang attr

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  future: { webpack5: true },
   reactStrictMode: true,
+  future: { webpack5: true },
+  i18n: { locales: ['en-US'], defaultLocale: 'en-US' },
   images: {
     domains: [
       'assets.tutorbook.org',
