73f962a47a70de85cf5ef60b8be183313aab5a9f
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-05-18 21:37:39 -0700

    feat(components/page): add social media meta tags

--- components/page.tsx
+++ components/page.tsx
@@ -6,27 +6,43 @@ import segmentSnippet from 'lib/segment-snippet';
 
 export interface PageProps {
   title: string;
-  description?: string;
   children: ReactNode;
 }
 
-export default function Page({
-  title,
-  description,
-  children,
-}: PageProps): JSX.Element {
+export default function Page({ title, children }: PageProps): JSX.Element {
   return (
     <>
       <Head>
         <title>{title}</title>
-        {description && <meta name='description' content={description} />}
+        <meta
+          content='A place where you can enjoy reading and learning from newsletters. So you spend less time in your inbox.'
+          name='description'
+        />
         <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width,initial-scale=1.0' />
+        <meta content='Hammock App - Newsletter Reader' property='og:title' />
+        <meta
+          content='A place where you can enjoy reading and learning from newsletters. So you spend less time in your inbox.'
+          property='og:description'
+        />
+        <meta content='/images/hammock-app.png' property='og:image' />
+        <meta
+          content='Hammock App - Newsletter Reader'
+          property='twitter:title'
+        />
+        <meta
+          content='A place where you can enjoy reading and learning from newsletters. So you spend less time in your inbox.'
+          property='twitter:description'
+        />
+        <meta content='/images/hammock-app.png' property='twitter:image' />
+        <meta property='og:type' content='website' />
+        <meta content='summary_large_image' name='twitter:card' />
         <link
-          href='https://uploads-ssl.webflow.com/5fd7d136cd029b7af8bdca15/5ff7c15e2096043b6eee8132_newsletter-favicon.png'
+          href='/images/favicon.png'
           rel='shortcut icon'
           type='image/x-icon'
         />
+        <link href='/images/webclip.png' rel='apple-touch-icon' />
         <link rel='preconnect' href='https://cdn.segment.com' />
         <link rel='preconnect' href='https://api.segment.io' />
         <script dangerouslySetInnerHTML={{ __html: segmentSnippet }} />
