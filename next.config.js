e2e411d070a8a59841d1bba64357003edc98c5e1
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-05-19 21:02:07 -0700

    feat(next.config.js): add legal and story redirects

--- next.config.js
+++ next.config.js
@@ -9,6 +9,28 @@ module.exports = {
       'www.google.com',
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/terms',
+        destination:
+          'https://www.notion.so/Terms-of-Service-ab660e15db814a118d908391eac43991',
+        permanent: true,
+      },
+      {
+        source: '/privacy',
+        destination:
+          'https://www.notion.so/Privacy-Policy-4f8a21493d384592a206453492fbce4c',
+        permanent: true,
+      },
+      {
+        source: '/story',
+        destination:
+          'https://www.notion.so/readhammock/Return-of-the-Newsletter-524563869f6242baaa60250299536654',
+        permanent: true,
+      },
+    ];
+  },
   webpack(config, { isServer }) {
     if (!isServer && process.env.ANALYZE === 'true') {
       // Only run the bundle analyzer for the client-side chunks.
