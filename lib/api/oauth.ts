74befe09025ae408543c5e0a26f0a34b8f6dae1f
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-06-09 20:07:32 +0000

    chore(env): remove unused `REDIRECT_URI` vars

--- lib/api/oauth.ts
+++ lib/api/oauth.ts
@@ -3,5 +3,4 @@ import { google } from 'googleapis';
 export default new google.auth.OAuth2(
   process.env.OAUTH_CLIENT_ID,
   process.env.OAUTH_CLIENT_SECRET,
-  process.env.NEXT_PUBLIC_OAUTH_REDIRECT_URI
 );
