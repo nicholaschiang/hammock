fc49c2fe5e0e068f78d5b7fa91d99ebd22770a0b
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-06-10 03:51:55 +0000

    fix(pages/archive): use new layout component

--- pages/archive.tsx
+++ pages/archive.tsx
@@ -1,24 +1,13 @@
-import NavBar from 'components/nav-bar';
-import Page from 'components/page';
 import Feed from 'components/feed';
+import Layout from 'components/layout';
+import Page from 'components/page';
 
 export default function ArchivePage(): JSX.Element {
   return (
     <Page name='Archive' login sync>
-      <div className='page'>
-        <NavBar />
+      <Layout>
         <Feed archive='true' />
-        <style jsx>{`
-          .page {
-            display: flex;
-            flex-direction: row;
-            justify-content: space-between;
-            max-width: 1048px;
-            padding: 0 48px;
-            margin: 96px auto;
-          }
-        `}</style>
-      </div>
+      </Layout>
     </Page>
   );
 }
