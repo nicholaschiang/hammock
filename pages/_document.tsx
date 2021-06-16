3a858d93528971d0409a34b07da2a27dd4f794f4
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-06-16 20:50:34 +0000

    feat(pages/document): change font display to `swap`

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default class Document extends NextDocument {
         <Head>
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
-            href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=optional'
+            href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap'
             rel='stylesheet'
           />
         </Head>
