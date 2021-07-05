86478c10f4ae76588fb382a021af46c88ed4e1b1
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-07-05 15:30:52 -0600

    feat: email notification with new newsletters (#82)
    * deps: add SendGrid email client Node.js SDK
    
    * wip(pages/api/notify): add notification endpoint
    
    * deps: add `@types/react-dom` dev dep
    
    * feat(api/notify): create MVP notification flow
    
    * feat(api/notify): require authorization token
    
    * feat: split email template from API route
    
    * feat(api/notify): update email subject and bcc

---
+++ components/email.tsx
@@ -0,0 +1,222 @@
+import { Message } from 'lib/model/message';
+import { User } from 'lib/model/user';
+
+const fontFamily = [
+  '-apple-system',
+  'BlinkMacSystemFont',
+  '"Segoe UI"',
+  '"Roboto"',
+  '"Oxygen"',
+  '"Ubuntu"',
+  '"Cantarell"',
+  '"Fira Sans"',
+  '"Droid Sans"',
+  '"Helvetica Neue"',
+  'sans-serif',
+].join(',');
+const colors = {
+  background: '#ffffff',
+  onBackground: '#000000',
+  accents1: '#fafafa',
+  accents2: '#eaeaea',
+  accents3: '#999999',
+  accents4: '#888888',
+  accents5: '#666666',
+  accents6: '#444444',
+};
+
+interface MessagesProps {
+  messages: Message[];
+}
+
+function Messages({ messages }: MessagesProps): JSX.Element {
+  return (
+    <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
+      {messages.map((message) => (
+        <a
+          style={{ textDecoration: 'none' }}
+          href={`https://readhammock.com/messages/${message.id}`}
+        >
+          <li key={message.id} style={{ margin: '40px 0' }}>
+            <p
+              style={{
+                fontFamily,
+                fontSize: '16px',
+                lineHeight: '24px',
+                overflow: 'hidden',
+                whiteSpace: 'nowrap',
+                textOverflow: 'ellipsis',
+                color: colors.accents5,
+                margin: 0,
+              }}
+            >
+              <img
+                style={{
+                  marginRight: '8px',
+                  borderRadius: '100%',
+                  verticalAlign: 'middle',
+                  border: `1px solid ${colors.accents5}`,
+                }}
+                src={message.from.photo}
+                height={24}
+                width={24}
+                alt=''
+              />
+              {message.from.name}
+            </p>
+            <h3
+              style={{
+                fontFamily,
+                fontSize: '18px',
+                lineHeight: 1,
+                overflow: 'hidden',
+                whiteSpace: 'nowrap',
+                textOverflow: 'ellipsis',
+                color: colors.accents6,
+                margin: '12px 0 8px',
+              }}
+            >
+              {message?.subject}
+            </h3>
+            <p
+              style={{
+                fontFamily,
+                fontSize: '16px',
+                lineHeight: 1.65,
+                color: colors.accents6,
+                margin: 0,
+              }}
+            >
+              {message.snippet}
+            </p>
+          </li>
+        </a>
+      ))}
+    </ul>
+  );
+}
+
+interface LinkProps {
+  href: string;
+  children: string;
+}
+
+function Link({ href, children }: LinkProps): JSX.Element {
+  return (
+    <a
+      href={href}
+      style={{
+        fontFamily,
+        color: '#067df7 !important',
+        textDecoration: 'none',
+      }}
+    >
+      {children}
+    </a>
+  );
+}
+
+export interface EmailProps {
+  user: User;
+  messages: Message[];
+}
+
+export default function Email({ user, messages }: EmailProps): JSX.Element {
+  // TODO: Ensure that this email formatting works both in the browser (b/c I'm
+  // reusing the same `Section` component) and in Gmail (we only have to care
+  // about the Gmail email client b/c we know all our users are on Gmail).
+  return (
+    <div style={{ backgroundColor: colors.background, padding: '24px' }}>
+      <table
+        style={{
+          borderCollapse: 'collapse',
+          borderSpacing: 0,
+          tableLayout: 'fixed',
+          width: '100%',
+          display: 'table',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          maxWidth: '580px',
+          padding: 0,
+          verticalAlign: 'top',
+        }}
+      >
+        <tbody>
+          <tr style={{ padding: '0px !important', margin: '0px !important' }}>
+            <td>
+              <div
+                style={{
+                  borderRadius: '12px',
+                  border: `1px solid ${colors.accents2}`,
+                  overflow: 'hidden',
+                  paddingLeft: '40px',
+                  paddingRight: '40px',
+                }}
+              >
+                <p
+                  style={{
+                    fontFamily,
+                    fontSize: '18px',
+                    lineHeight: 1.65,
+                    color: colors.onBackground,
+                    margin: '40px 0 0',
+                  }}
+                >
+                  Hi {user.firstName},
+                </p>
+                <p
+                  style={{
+                    fontFamily,
+                    fontSize: '18px',
+                    lineHeight: 1.65,
+                    color: colors.onBackground,
+                    margin: '8px 0',
+                  }}
+                >
+                  Here’s what’s new in Hammock today:
+                </p>
+                <Messages messages={messages} />
+                <hr
+                  style={{
+                    border: 'none',
+                    borderTop: `1px solid ${colors.accents2}`,
+                    margin: '26px 0',
+                    width: '100%',
+                  }}
+                />
+                <p
+                  style={{
+                    fontFamily,
+                    fontSize: '16px',
+                    lineHeight: 1.65,
+                    color: colors.accents5,
+                    margin: '8px 0',
+                  }}
+                >
+                  Hammock -{' '}
+                  <Link href='https://readhammock.com'>readhammock.com</Link>
+                </p>
+                <p
+                  style={{
+                    fontFamily,
+                    fontSize: '16px',
+                    lineHeight: 1.65,
+                    color: colors.accents5,
+                    margin: '8px 0 40px',
+                  }}
+                >
+                  If this message contains spam or unwanted messages let us know
+                  at{' '}
+                  <Link href='mailto:team@readhammock.com'>
+                    team@readhammock.com
+                  </Link>{' '}
+                  or by simply replying to this email.
+                </p>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
---
+++ pages/api/notify.tsx
@@ -0,0 +1,86 @@
+import { NextApiRequest as Req, NextApiResponse as Res } from 'next';
+import mail, { MailDataRequired } from '@sendgrid/mail';
+import { renderToStaticMarkup } from 'react-dom/server';
+
+import Email from 'components/email';
+
+import { APIError, APIErrorJSON } from 'lib/model/error';
+import { Message } from 'lib/model/message';
+import { User } from 'lib/model/user';
+import { db } from 'lib/api/firebase';
+import { handle } from 'lib/api/error';
+import logger from 'lib/api/logger';
+
+/**
+ * GET - Sends notification emails with a summary of all the new newsletters in
+ *       the user's feed (from the last 24 hours).
+ *
+ * Requires a specific authorization token known only by our CRON job.
+ */
+export default async function notifyAPI(
+  req: Req,
+  res: Res<APIErrorJSON>
+): Promise<void> {
+  if (req.method !== 'GET') {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).end(`Method ${req.method as string} Not Allowed`);
+  } else {
+    try {
+      if (typeof req.query.token !== 'string')
+        throw new APIError('Missing authentication token', 401);
+      if (req.query.token !== process.env.NOTIFY_TOKEN)
+        throw new APIError('Invalid authentication token', 401);
+      const now = new Date();
+      const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+      logger.info('Fetching users...');
+      const { docs: users } = await db.collection('users').get();
+      const emails: MailDataRequired[] = [];
+      logger.info(`Fetching messages for ${users.length} users...`);
+      await Promise.all(
+        users.map(async (doc) => {
+          // TODO: Sync the latest Gmail messages before fetching these.
+          // TODO: Filter for message dates that are today in the user's time zone
+          // (this will require us to store user time zones in our db).
+          const user = User.fromFirestoreDoc(doc);
+          logger.verbose(`Fetching messages for ${user}...`);
+          const { docs } = await doc.ref
+            .collection('messages')
+            .where('date', '>=', today)
+            .orderBy('date', 'desc')
+            .limit(10)
+            .get();
+          const messages = docs.map((d) => Message.fromFirestoreDoc(d));
+          if (!messages.length) {
+            logger.verbose(
+              `Skipping email for ${user} with no new messages...`
+            );
+          } else {
+            logger.verbose(
+              `Queuing email for ${user} with ${messages.length} new messages...`
+            );
+            emails.push({
+              to: { name: user.name, email: user.email },
+              from: { name: 'Hammock', email: 'team@readhammock.com' },
+              bcc: { name: 'Hammock', email: 'team@readhammock.com' },
+              subject: `Read in Hammock: ${messages
+                .slice(0, 3)
+                .map((m) => m.from.name)
+                .join(', ')}`,
+              html: renderToStaticMarkup(
+                <Email user={user} messages={messages} />
+              ),
+            });
+          }
+        })
+      );
+      logger.info(`Sending ${emails.length} emails...`);
+      if (typeof process.env.SENDGRID_API_KEY !== 'string')
+        throw new APIError('Missing SendGrid API key', 401);
+      mail.setApiKey(process.env.SENDGRID_API_KEY);
+      await mail.send(emails);
+      res.status(200).end();
+    } catch (e) {
+      handle(e, res);
+    }
+  }
+}
