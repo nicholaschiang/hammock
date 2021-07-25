e471d20f645834d28628384c98664527217dbd6c
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-07-24 17:15:24 -0700

    chore(scripts): backup Firestore as JSON file

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -1,7 +1,21 @@
-const db = require('./db');
+process.env.NODE_ENV = 'development';
+const dev = require('./db');
+
+process.env.NODE_ENV = 'production';
+const prod = require('./db');
 
 async function migrate() {
-  const { docs } = await db.collection('users').get();
+  const { docs } = await prod.collection('users').get();
+  const batch = dev.batch();
+  docs.map((doc) => {
+    const data = doc.data();
+    data.email = `${data.email.split('@')[0]}@example.com`;
+    const ref = dev.collection('users').doc(doc.id);
+    debugger;
+    batch.set(ref, data);
+  });
+  debugger;
+  await batch.commit();
 }
 
 if (require.main === module) migrate();
