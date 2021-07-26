b9037db3e489d105f9669ae474e4fb1b6da6a74a
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-07-26 12:29:26 -0700

    wip: use new SWR `getInfiniteKey` API

--- lib/hooks/messages.ts
+++ lib/hooks/messages.ts
@@ -3,28 +3,31 @@ import {
   SWRInfiniteResponse,
   useSWRInfinite,
 } from 'swr';
-import { useCallback } from 'react';
+import { useMemo } from 'react';
 
 import { MessagesQuery, MessagesRes } from 'pages/api/messages';
 
 import { APIError } from 'lib/model/error';
 
+export function getKeyFunction(
+  query: MessagesQuery = {}
+): (pageIdx: number, prev: MessagesRes | null) => string | null {
+  return (pageIdx: number, prev: MessagesRes | null) => {
+    const params = new URLSearchParams(query);
+    if (prev && !prev.length) return null;
+    if (!prev || pageIdx === 0) {
+      const queryString = params.toString();
+      return queryString ? `/api/messages?${queryString}` : '/api/messages';
+    }
+    params.append('lastMessageId', prev[prev.length - 1].id);
+    return `/api/messages?${params.toString()}`;
+  };
+}
+
 export default function useMessages(
   query: MessagesQuery = {},
   config: SWRInfiniteConfiguration = { revalidateAll: true }
 ): SWRInfiniteResponse<MessagesRes> {
-  const getKey = useCallback(
-    (pageIdx: number, prev: MessagesRes | null) => {
-      const params = new URLSearchParams(query);
-      if (prev && !prev.length) return null;
-      if (!prev || pageIdx === 0) {
-        const queryString = params.toString();
-        return queryString ? `/api/messages?${queryString}` : '/api/messages';
-      }
-      params.append('lastMessageId', prev[prev.length - 1].id);
-      return `/api/messages?${params.toString()}`;
-    },
-    [query]
-  );
+  const getKey = useMemo(() => getKeyFunction(query), [query]);
   return useSWRInfinite<MessagesRes, APIError>(getKey, config);
 }
