e996f33020245f9adce913c7ba61ddd63f36d6f6
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-07-29 11:08:56 -0700

    feat(article): add highlight selection dialog

--- components/article.tsx
+++ components/article.tsx
@@ -1,43 +1,96 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import cn from 'classnames';
+
+import HighlightIcon from 'components/icons/highlight';
+import NoteIcon from 'components/icons/note';
+import TweetIcon from 'components/icons/tweet';
 
 import { XPath, fromNode } from 'lib/xpath';
 import { Message } from 'lib/model/message';
 import highlight from 'lib/highlight';
+
+interface Position {
+  x: number;
+  y: number;
+  containerX: number;
+  containerY: number;
+}
 
 export interface ArticleProps {
   message?: Message;
 }
 
 export default function Article({ message }: ArticleProps): JSX.Element {
+  const [xpath, setXPath] = useState<XPath>();
   const [xpaths, setXPaths] = useState<XPath[]>([]);
+  const [position, setPosition] = useState<Position>();
+  const [selection, setSelection] = useState<string>('');
   const ref = useRef<HTMLElement>(null);
   useEffect(() => {
-    function listener(): void {
+    function listener(evt: MouseEvent): void {
       const sel = window.getSelection() || document.getSelection();
       const range = sel?.getRangeAt(0);
       console.log('Range:', range);
-      setXPaths((prev) => {
-        if (!range || !ref.current) return prev;
+      setSelection(range?.toString() || '');
+      setPosition({
+        x: evt.offsetX,
+        y: evt.offsetY,
+        containerX: evt.target.offsetLeft,
+        containerY: evt.target.offsetTop,
+      });
+      setXPath(() => {
+        if (!range || range.collapsed || !ref.current) return undefined;
         const { startContainer, endContainer, startOffset, endOffset } = range;
         const start = fromNode(startContainer, ref.current);
-        // TODO: Determine dialog position based on the end container's x and y
-        // coordinates relative to the viewport. Then, simply store those in a
-        // state object and use them dynamically to style a dialog pop-up.
         const end = fromNode(endContainer, ref.current);
-        window.endContainer = endContainer;
-        return [...prev, { start, end, startOffset, endOffset }];
+        return { start, end, startOffset, endOffset };
       });
     }
     window.addEventListener('mouseup', listener);
     return () => window.removeEventListener('mouseup', listener);
   }, []);
+  useEffect(() => console.log('Position:', position), [position]);
   const html = useMemo(
     () => (message ? highlight(message.html, xpaths) : ''),
     [xpaths, message]
   );
+  const onHighlight = useCallback(() => {
+    setXPaths((prev) => {
+      if (!xpath) return prev;
+      return [...prev, xpath];
+    });
+  }, [xpath]);
+  const onNote = useCallback(() => {
+    console.log('TODO: Implement notes.');
+  }, []);
 
   return (
     <>
+      <div className={cn('dialog', { open: position && xpath })}>
+        <div className='buttons'>
+          <button className='reset button' type='button' onClick={onHighlight}>
+            <HighlightIcon />
+          </button>
+          <button className='reset button' type='button' onClick={onNote}>
+            <NoteIcon />
+          </button>
+          <a
+            className='reset button'
+            href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+              `“${selection}” — ${message?.from.name || 'Newsletter'}`
+            )}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <TweetIcon />
+          </a>
+        </div>
+        <div className='shadows'>
+          <div className='shadow' />
+          <div className='shadow' />
+          <div className='shadow' />
+        </div>
+      </div>
       {message && (
         <article ref={ref} dangerouslySetInnerHTML={{ __html: html }} />
       )}
@@ -49,6 +102,103 @@ export default function Article({ message }: ArticleProps): JSX.Element {
         </article>
       )}
       <style jsx>{`
+        .dialog {
+          position: absolute;
+          visibility: hidden;
+          left: ${position?.containerX || 0}px;
+          top: ${position?.containerY || 0}px;
+        }
+
+        .dialog.open {
+          visibility: visible;
+        }
+
+        .buttons,
+        .shadows {
+          position: absolute;
+          display: flex;
+          left: ${position?.x || 0}px;
+          top: ${position?.y || 0}px;
+        }
+
+        .button {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background: var(--background);
+          pointer-events: initial;
+          z-index: 4;
+        }
+
+        .dialog .button,
+        .dialog .shadow {
+          opacity: 0;
+          transform: translateY(-5px);
+        }
+
+        .dialog.open .button,
+        .dialog.open .shadow {
+          opacity: 1;
+          transform: translateY(0px);
+        }
+
+        .button:nth-of-type(1),
+        .shadow:nth-of-type(1) {
+          transition: background 0.2s ease 0s, transform 0.2s ease-out 0s,
+            opacity 0.2s ease-out 0s;
+        }
+
+        .button:nth-of-type(2),
+        .shadow:nth-of-type(2) {
+          transition: background 0.2s ease 0s, transform 0.2s ease-out 0.05s,
+            opacity 0.2s ease-out 0.05s;
+        }
+
+        .button:nth-of-type(3),
+        .shadow:nth-of-type(3) {
+          transition: background 0.2s ease 0s, transform 0.2s ease-out 0.1s,
+            opacity 0.2s ease-out 0.1s;
+        }
+
+        .shadow,
+        .button {
+          margin: 0 2px;
+          width: 35px;
+          height: 35px;
+          border-radius: 25px;
+        }
+
+        .shadow {
+          box-shadow: var(--shadow-medium);
+        }
+
+        .shadow:first-child,
+        .button:first-child {
+          margin-left: unset;
+        }
+
+        .shadow:last-child,
+        .button:last-child {
+          margin-right: unset;
+        }
+
+        .button:hover {
+          background: var(--accents-2);
+        }
+
+        .button:disabled {
+          cursor: not-allowed;
+        }
+
+        .button :global(svg) {
+          fill: var(--accents-5);
+          transition: fill 0.2s ease 0s;
+        }
+
+        .button:hover :global(svg) {
+          fill: var(--on-background);
+        }
+
         p.loading {
           border-radius: 6px;
         }
---
+++ components/icons/highlight.tsx
@@ -0,0 +1,10 @@
+export default function Highlight(): JSX.Element {
+  return (
+    <svg width='25' height='25' viewBox='0 0 25 25'>
+      <path
+        d='M13.7 15.96l5.2-9.38-4.72-2.62-5.2 9.38 4.72 2.62zm-.5.89l-1.3 2.37-1.26.54-.7 1.26-3.8-.86 1.23-2.22-.2-1.35 1.31-2.37 4.73 2.62z'
+        fill-rule='evenodd'
+      ></path>
+    </svg>
+  );
+}
