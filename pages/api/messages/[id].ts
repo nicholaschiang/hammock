78d52271f3ccd17ea15c7e5b1b5defbcbc46966a
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-08-15 16:25:40 -0700

    feat(api/messages/id): replace Firestore with Supabase

--- pages/api/messages/[id].ts
+++ pages/api/messages/[id].ts
@@ -1,12 +1,11 @@
 import { NextApiRequest as Req, NextApiResponse as Res } from 'next';
 
 import { Message, MessageJSON, isMessageJSON } from 'lib/model/message';
+import { getMessage, updateMessage } from 'lib/api/db/message';
 import { APIErrorJSON } from 'lib/model/error';
-import { db } from 'lib/api/firebase';
 import { handle } from 'lib/api/error';
 import logger from 'lib/api/logger';
 import segment from 'lib/api/segment';
-import updateMessageDoc from 'lib/api/update/message-doc';
 import verifyAuth from 'lib/api/verify/auth';
 import verifyBody from 'lib/api/verify/body';
 import verifyQueryId from 'lib/api/verify/query-id';
@@ -21,13 +20,7 @@ async function fetchMessage(
     const id = verifyQueryId(req.query);
     console.time(`fetch-message-${id}`);
     const user = await verifyAuth(req);
-    const doc = await db
-      .collection('users')
-      .doc(user.id)
-      .collection('messages')
-      .doc(id)
-      .get();
-    const message = Message.fromFirestoreDoc(doc);
+    const message = await getMessage(id);
     res.status(200).json(message.toJSON());
     logger.info(`Fetched ${message} for ${user}.`);
     console.timeEnd(`fetch-message-${id}`);
@@ -52,13 +45,13 @@ async function updateMessage(
       Message
     );
     const user = await verifyAuth(req);
-    await updateMessageDoc(user.id, body);
-    res.status(200).json(body.toJSON());
-    logger.info(`Updated ${body} for ${user}.`);
+    const message = await updateMessage(body);
+    res.status(200).json(message.toJSON());
+    logger.info(`Updated ${message} for ${user}.`);
     segment.track({
       userId: user.id,
       event: 'Message Updated',
-      properties: body.toSegment(),
+      properties: message.toSegment(),
     });
   } catch (e) {
     handle(e, res);
