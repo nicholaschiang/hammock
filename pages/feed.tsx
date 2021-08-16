2804c775a35b58422fb05c66c3a27b4b6f764964
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-08-15 19:49:26 -0700

    fix(pages): remove temporary errors
    This reverts commit 423484c617e194b8f669b276fd1f77e862d5bb78.

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -1,14 +1,8 @@
-import { useEffect } from 'react';
-
 import Feed from 'components/feed';
 import Layout from 'components/layout';
 import Page from 'components/page';
 
 export default function FeedPage(): JSX.Element {
-  useEffect(() => {
-    throw new Error('This is a fake production client-side error!');
-  }, []);
-
   return (
     <Page name='Feed' login sync>
       <Layout spacer>
