f8f344fd497f008401d2db6268cabacacb66d404
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-08-15 19:10:40 -0700

    chore(next.config.js): suppress Sentry logs
    See: https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#extend-nextjs-configuration

--- next.config.js
+++ next.config.js
@@ -1,76 +1,79 @@
 const { withSentryConfig } = require('@sentry/nextjs');
 
-module.exports = withSentryConfig({
-  reactStrictMode: true,
-  i18n: { locales: ['en-US'], defaultLocale: 'en-US' },
-  images: {
-    domains: [
-      'cdn.substack.com',
-      'assets.tutorbook.org',
-      'lh3.googleusercontent.com',
-      'www.google.com',
-    ],
+module.exports = withSentryConfig(
+  {
+    reactStrictMode: true,
+    i18n: { locales: ['en-US'], defaultLocale: 'en-US' },
+    images: {
+      domains: [
+        'cdn.substack.com',
+        'assets.tutorbook.org',
+        'lh3.googleusercontent.com',
+        'www.google.com',
+      ],
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/about',
+          destination: '/',
+        },
+      ];
+    },
+    async redirects() {
+      return [
+        {
+          source: '/terms',
+          destination:
+            'https://readhammock.notion.site/Terms-of-Service-ab660e15db814a118d908391eac43991',
+          permanent: true,
+        },
+        {
+          source: '/privacy',
+          destination:
+            'https://readhammock.notion.site/Privacy-Policy-4f8a21493d384592a206453492fbce4c',
+          permanent: true,
+        },
+        {
+          source: '/story',
+          destination:
+            'https://readhammock.notion.site/Return-of-the-Newsletter-524563869f6242baaa60250299536654',
+          permanent: true,
+        },
+        {
+          source: '/changelog',
+          destination:
+            'https://readhammock.notion.site/Changelog-565a632fc5e3466d85748a78ddeae8f6',
+          permanent: true,
+        },
+        {
+          source: '/waitlist',
+          destination: 'https://form.typeform.com/to/PezpoULN',
+          permanent: true,
+        },
+        {
+          source: '/try',
+          destination:
+            'https://readhammock.notion.site/Hi-we-re-building-Hammock-1d9ac6c2e6004974ac1eafd00a53b4cd',
+          permanent: true,
+        },
+      ];
+    },
+    webpack(config, { isServer }) {
+      if (!isServer && process.env.ANALYZE === 'true') {
+        // Only run the bundle analyzer for the client-side chunks.
+        // @see {@link https://github.com/vercel/next.js/issues/15481}
+        const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+        config.plugins.push(
+          new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            reportFilename: './analyze/client.html',
+            generateStatsFile: true,
+          })
+        );
+      }
+      return config;
+    },
   },
-  async rewrites() {
-    return [
-      {
-        source: '/about',
-        destination: '/',
-      },
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: '/terms',
-        destination:
-          'https://readhammock.notion.site/Terms-of-Service-ab660e15db814a118d908391eac43991',
-        permanent: true,
-      },
-      {
-        source: '/privacy',
-        destination:
-          'https://readhammock.notion.site/Privacy-Policy-4f8a21493d384592a206453492fbce4c',
-        permanent: true,
-      },
-      {
-        source: '/story',
-        destination:
-          'https://readhammock.notion.site/Return-of-the-Newsletter-524563869f6242baaa60250299536654',
-        permanent: true,
-      },
-      {
-        source: '/changelog',
-        destination:
-          'https://readhammock.notion.site/Changelog-565a632fc5e3466d85748a78ddeae8f6',
-        permanent: true,
-      },
-      {
-        source: '/waitlist',
-        destination: 'https://form.typeform.com/to/PezpoULN',
-        permanent: true,
-      },
-      {
-        source: '/try',
-        destination:
-          'https://readhammock.notion.site/Hi-we-re-building-Hammock-1d9ac6c2e6004974ac1eafd00a53b4cd',
-        permanent: true,
-      },
-    ];
-  },
-  webpack(config, { isServer }) {
-    if (!isServer && process.env.ANALYZE === 'true') {
-      // Only run the bundle analyzer for the client-side chunks.
-      // @see {@link https://github.com/vercel/next.js/issues/15481}
-      const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-      config.plugins.push(
-        new BundleAnalyzerPlugin({
-          analyzerMode: 'static',
-          reportFilename: './analyze/client.html',
-          generateStatsFile: true,
-        })
-      );
-    }
-    return config;
-  },
-});
+  { silent: true }
+);
