e7d984b7fb2928cfa1030e5802b04460d4b45c40
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-08-18 18:26:56 -0700

    test: finalize index page tests

--- cypress/integration/index.spec.ts
+++ cypress/integration/index.spec.ts
@@ -1,9 +1,21 @@
 describe('Index page', () => {
-  beforeEach(() => {
+  it('shows screenshot and benefits', () => {
     cy.visit('/');
+    cy.percySnapshot('Index Page');
   });
 
-  it('shows screenshot and benefits', () => {
-    cy.percySnapshot('Index Page');
+  it('redirects to feed when logged in', () => {
+    cy.login();
+    cy.visit('/');
+    cy.wait('@get-account');
+    cy.url().should('include', '/feed');
+  });
+
+  it('skips redirect when on /about page', () => {
+    cy.login();
+    cy.visit('/about');
+    cy.wait('@get-account');
+    cy.url().should('not.include', '/feed');
+    cy.percySnapshot('About Page');
   });
 });
--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,2 +1,15 @@
 import '@cypress/code-coverage/support';
 import '@percy/cypress';
+
+Cypress.Commands.add('login', () => {
+  cy.intercept('GET', '/api/account', { fixture: 'user' }).as('get-account');
+});
+
+declare global {
+  /* eslint-disable-next-line @typescript-eslint/no-namespace */
+  namespace Cypress {
+    interface Chainable {
+      login: () => Chainable<undefined>;
+    }
+  }
+}
