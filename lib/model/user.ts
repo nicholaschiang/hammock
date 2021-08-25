f551de7790b4e4b62c4c6961932a98ba0f7cfee2
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-08-24 22:01:28 -0700

    fix(model/user): allow `null` fields in validator

--- lib/model/user.ts
+++ lib/model/user.ts
@@ -1,5 +1,5 @@
 import { Subscription, isSubscription } from 'lib/model/subscription';
-import { isArray, isJSON } from 'lib/model/json';
+import { isArray, isJSON, isStringArray } from 'lib/model/json';
 import { APIError } from 'lib/model/error';
 
 export const SCOPES = {
@@ -25,20 +25,27 @@ export interface User {
 }
 
 export function isUser(user: unknown): user is User {
-  const stringFields = [
-    'id',
-    'name',
-    'photo',
-    'locale',
-    'email',
-    'phone',
-    'token',
-    'label',
-    'filter',
-  ];
   if (!isJSON(user)) throw new APIError('Expected valid JSON body', 400);
-  if (stringFields.some((key) => typeof user[key] !== 'string'))
-    throw new APIError('Expected valid string field', 400);
+  if (typeof user.id !== 'number')
+    throw new APIError('Expected valid number ID field', 400);
+  if (typeof user.name !== 'string')
+    throw new APIError('Expected valid string name field', 400);
+  if (typeof user.photo !== 'string' && user.photo !== null)
+    throw new APIError('Expected valid string or null photo field', 400);
+  if (typeof user.email !== 'string' && user.email !== null)
+    throw new APIError('Expected valid string or null email field', 400);
+  if (typeof user.phone !== 'string' && user.phone !== null)
+    throw new APIError('Expected valid string or null phone field', 400);
+  if (typeof user.locale !== 'string')
+    throw new APIError('Expected valid string locale field', 400);
+  if (typeof user.token !== 'string')
+    throw new APIError('Expected valid string token field', 400);
+  if (!isStringArray(user.scopes))
+    throw new APIError('Expected valid subscriptions', 400);
+  if (typeof user.label !== 'string')
+    throw new APIError('Expected valid string label field', 400);
+  if (typeof user.filter !== 'string')
+    throw new APIError('Expected valid string filter field', 400);
   if (!isArray(user.subscriptions, isSubscription))
     throw new APIError('Expected valid subscriptions', 400);
   return true;
