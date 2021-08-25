91ffd59bf4c4062e35b4f0aa781772cd07d2a801
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-08-24 21:29:35 -0700

    chore(scripts): add `url` and `email` validations

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -10,6 +10,25 @@ const SCOPES = {
   FILTER: 'https://www.googleapis.com/auth/gmail.settings.basic',
 };
 
+function email(email, required = false) {
+  if (!required && email === null) return null;
+  if (/^[A-Za-z0-9._~+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$/.test(email))
+    return email;
+  logger.error(`Invalid email: ${email}`);
+  debugger;
+  throw new Error(`Invalid email: ${email}`);
+}
+
+function url(url, required = false) {
+  if (!required && url === null) return null;
+  if (typeof url !== 'string') return null;
+  if (url.startsWith('/')) url = `https://readhammock.com${url}`;
+  if (/^https?:\/\/\S+$/.test(url.trim())) return url.trim();
+  logger.error(`Invalid URL: ${url}`);
+  debugger;
+  throw new Error(`Invalid URL: ${url}`);
+}
+
 // Adds the full `scopes` property to each of our users.
 async function migrate() {
   logger.info('Fetching user docs...');
@@ -20,18 +39,25 @@ async function migrate() {
     .map((d) => ({
       id: Number(d.id),
       name: d.name,
-      photo: d.photo || null,
-      email: d.email || null,
+      photo: url(d.photo || null),
+      email: email(d.email || null),
       phone: d.phone || null,
       locale: d.locale || 'en-US',
       token: d.token || '',
       scopes: d.scopes || Object.values(SCOPES),
       label: d.label || '',
       filter: d.filter || '',
-      subscriptions: d.subscriptions || [],
+      subscriptions: (d.subscriptions || []).map((s) => ({
+        name: s.from.name,
+        email: email(s.from.email),
+        photo: url(s.from.photo),
+        category: s.category,
+        favorite: s.favorite,
+      })),
     }));
   logger.verbose(`Inserting ${docs.length} user rows...`);
   const { data, error } = await supabase.from('users').insert(users);
+  if (error) logger.error(`Error inserting user rows: ${error.message}`);
   /*
    *const messages = [];
    *const highlights = [];
