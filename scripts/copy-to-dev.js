ac739712fa4ba7e147de06166f878a7943f63b3d
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-09-04 13:04:45 -0700

    chore(scripts/copy-to-dev): adapt for Supabase

--- scripts/copy-to-dev.js
+++ scripts/copy-to-dev.js
@@ -1,21 +1,32 @@
-const dev = require('./db')('development');
-const prod = require('./db')('production');
+const dev = require('./supabase')('development');
+const prod = require('./supabase')('production');
 const logger = require('./logger');
 
 // Copies the users from our production database to our development database
 // and replaces their email addresses with `@example.com` ones.
 async function copyToDev() {
-  const { docs } = await prod.collection('users').get();
-  const batch = dev.batch();
-  docs.map((doc) => {
-    const data = doc.data();
-    data.email = `${data.email.split('@')[0]}@example.com`;
-    const ref = dev.collection('users').doc(doc.id);
-    logger.verbose(`Adding ${data.name} <${data.email}> to batch...`);
-    batch.set(ref, data);
-  });
-  logger.info(`Committing batch of ${docs.length} updates...`);
-  await batch.commit();
+  const { data, error } = await prod.from('users').select();
+  if (error) {
+    logger.error(`Error fetching users: ${error.message}`);
+    debugger;
+  } else if (!data) {
+    logger.error(`Missing data: ${data}`);
+    debugger;
+  } else {
+    logger.info(`Saving ${data.length} users...`);
+    const { error } = await dev
+      .from('users')
+      .insert(
+        data.map((d) => ({
+          ...d,
+          email: `${d.email.split('@')[0]}@example.com`,
+        }))
+      );
+    if (error) {
+      logger.error(`Error inserting users: ${error.message}`);
+      debugger;
+    }
+  }
 }
 
 if (require.main === module) copyToDev();
