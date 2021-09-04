d676a6a4f3d1220f17ff999f6d7555f5a2788ea8
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-09-04 13:14:29 -0700

    fix(lib/api/gmail): don't reuse `OAuth` client

--- lib/api/gmail/index.ts
+++ lib/api/gmail/index.ts
@@ -1,11 +1,13 @@
 import { gmail_v1, google } from 'googleapis';
 
-import oauth2Client from 'lib/api/oauth';
-
 export type Gmail = gmail_v1.Gmail;
 export type GmailMessage = gmail_v1.Schema$Message;
 
 export default function gmail(token: string): Gmail {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.OAUTH_CLIENT_ID,
+    process.env.OAUTH_CLIENT_SECRET
+  );
   oauth2Client.setCredentials({ refresh_token: token });
   return google.gmail({ auth: oauth2Client, version: 'v1' });
 }
--- lib/api/oauth.ts
+++
@@ -1,6 +0,0 @@
-import { google } from 'googleapis';
-
-export default new google.auth.OAuth2(
-  process.env.OAUTH_CLIENT_ID,
-  process.env.OAUTH_CLIENT_SECRET,
-);
